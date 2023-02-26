2d3382ade9a8c805745c577361d8d7630d1cd8bd
John Wells <johnwells.developer@gmail.com>
2023-02-26 15:28:17 -0700

    lesson one complete

---
+++ Lesson-1/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Lesson 1: understanding the file structure, tsconfig file, -w and build files
+let username = "Johnny";
+console.log(username);
+let a = 12;
+let b = 6;
+let c = 2;
+console.log(a / b);
+console.log(c * b);
---
+++ Lesson-1/src/main.ts
@@ -0,0 +1,11 @@
+// Lesson 1: understanding the file structure, tsconfig file, -w and build files
+let username = "Johnny";
+console.log(username);
+
+let a: number = 12;
+let b: number = 6;
+let c: number = 2;
+
+console.log(a / b);
+
+console.log(c * b);
